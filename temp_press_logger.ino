61404ef9e1a74981693415648dd5ae55a5f7fbb3
Ralph Stevenson-Jones <ralph-sj@users.noreply.github.com>
2023-04-20 13:11:57 +0000

    Added switches for logging

--- temp_press_logger.ino
+++ temp_press_logger.ino
@@ -3,20 +3,14 @@
 #include <Wire.h>
 #include "RTClib.h"
 
-// A simple data logger for the Arduino analog pins
 
-// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
 #define LOG_INTERVAL  100 // mills between entries (reduce to take more/faster data)
-
-// how many milliseconds before writing the logged data permanently to disk
-// set it to the LOG_INTERVAL to write each time (safest)
-// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
-// the last 10 reads if power is lost but it uses less power and is much faster!
 #define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
 uint32_t syncTime = 0; // time of last sync()
 
-#define ECHO_TO_SERIAL   1 // echo data to serial port
-#define WAIT_TO_START    0 // Wait for serial input in setup()
+#define ECHO_TO_SERIAL   1
+#define WAIT_TO_START    0 
+#define PRINT_TO_FILE    1
 
 // the digital pins that connect to the LEDs
 #define redLEDpin 2
@@ -58,39 +52,42 @@ void setup(void)
   pinMode(redLEDpin, OUTPUT);
   pinMode(greenLEDpin, OUTPUT);
   
-#if WAIT_TO_START
-  Serial.println("Type any character to start");
-  while (!Serial.available());
-#endif //WAIT_TO_START
+  #if WAIT_TO_START
+    Serial.println("Type any character to start");
+    while (!Serial.available());
+  #endif //WAIT_TO_START
 
-  // initialize the SD card
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(10, OUTPUT);
+  #if PRINT_TO_FILE
+    // initialize the SD card
+    Serial.print("Initializing SD card...");
+    // make sure that the default chip select pin is set to
+    // output, even if you don't use it:
+    pinMode(10, OUTPUT);
   
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    error("Card failed, or not present");
-  }
-  Serial.println("card initialized.");
-  
-  // create a new file
-  char filename[] = "LOGGER00.CSV";
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[6] = i/10 + '0';
-    filename[7] = i%10 + '0';
-    if (! SD.exists(filename)) {
-      // only open a new file if it doesn't exist
-      logfile = SD.open(filename, FILE_WRITE); 
-      break;  // leave the loop!
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+      error("Card failed, or not present");
     }
-  }
+    Serial.println("card initialized.");
+  
+    // create a new file
+    char filename[] = "LOGGER00.CSV";
+    for (uint8_t i = 0; i < 100; i++) {
+      filename[6] = i/10 + '0';
+      filename[7] = i%10 + '0';
+      if (! SD.exists(filename)) {
+        // only open a new file if it doesn't exist
+        logfile = SD.open(filename, FILE_WRITE); 
+        break;  // leave the loop!
+      }
+     }
+  #endif //PRINT_TO_FILE
+
   
   if (! logfile) {
     error("couldnt create file");
   }
-  
+
   Serial.print("Logging to: ");
   Serial.println(filename);
 
@@ -98,19 +95,18 @@ void setup(void)
   Wire.begin();  
   if (!RTC.begin()) {
     logfile.println("RTC failed");
-#if ECHO_TO_SERIAL
-    Serial.println("RTC failed");
-#endif  //ECHO_TO_SERIAL
-  }
-  
+    #if ECHO_TO_SERIAL
+      Serial.println("RTC failed");
+    #endif  //ECHO_TO_SERIAL
+  } 
 
   logfile.println("millis,stamp,datetime,light,temp,vcc");    
-#if ECHO_TO_SERIAL
-  Serial.println("millis,stamp,datetime,light,temp,vcc");
-#endif //ECHO_TO_SERIAL
+  #if ECHO_TO_SERIAL
+    Serial.println("millis,stamp,datetime,light,temp,vcc");
+  #endif //ECHO_TO_SERIAL
  
   // If you want to set the aref to something other than 5v
-  analogReference(EXTERNAL);
+    analogReference(EXTERNAL);
 }
 
 void loop(void)
@@ -126,10 +122,10 @@ void loop(void)
   uint32_t m = millis();
   logfile.print(m);           // milliseconds since start
   logfile.print(", ");    
-#if ECHO_TO_SERIAL
-  Serial.print(m);         // milliseconds since start
-  Serial.print(", ");  
-#endif
+  #if ECHO_TO_SERIAL
+    Serial.print(m);         // milliseconds since start
+    Serial.print(", ");  
+  #endif
 
   // fetch the time
   now = RTC.now();
@@ -149,23 +145,23 @@ void loop(void)
   logfile.print(":");
   logfile.print(now.second(), DEC);
   logfile.print('"');
-#if ECHO_TO_SERIAL
-  Serial.print(now.unixtime()); // seconds since 1/1/1970
-  Serial.print(", ");
-  Serial.print('"');
-  Serial.print(now.year(), DEC);
-  Serial.print("/");
-  Serial.print(now.month(), DEC);
-  Serial.print("/");
-  Serial.print(now.day(), DEC);
-  Serial.print(" ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(":");
-  Serial.print(now.minute(), DEC);
-  Serial.print(":");
-  Serial.print(now.second(), DEC);
-  Serial.print('"');
-#endif //ECHO_TO_SERIAL
+  #if ECHO_TO_SERIAL
+    Serial.print(now.unixtime()); // seconds since 1/1/1970
+    Serial.print(", ");
+    Serial.print('"');
+    Serial.print(now.year(), DEC);
+    Serial.print("/");
+    Serial.print(now.month(), DEC);
+    Serial.print("/");
+    Serial.print(now.day(), DEC);
+    Serial.print(" ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(":");
+    Serial.print(now.minute(), DEC);
+    Serial.print(":");
+    Serial.print(now.second(), DEC);
+    Serial.print('"');
+  #endif //ECHO_TO_SERIAL
 
   analogRead(photocellPin);
   delay(10); 
@@ -184,12 +180,12 @@ void loop(void)
   logfile.print(photocellReading);
   logfile.print(", ");    
   logfile.print(temperatureF);
-#if ECHO_TO_SERIAL
-  Serial.print(", ");   
-  Serial.print(photocellReading);
-  Serial.print(", ");    
-  Serial.print(temperatureF);
-#endif //ECHO_TO_SERIAL
+  #if ECHO_TO_SERIAL
+    Serial.print(", ");   
+    Serial.print(photocellReading);
+    Serial.print(", ");    
+    Serial.print(temperatureF);
+  #endif //ECHO_TO_SERIAL
 
   // Log the estimated 'VCC' voltage by measuring the internal 1.1v ref
   analogRead(BANDGAPREF); 
@@ -199,15 +195,15 @@ void loop(void)
   
   logfile.print(", ");
   logfile.print(supplyvoltage);
-#if ECHO_TO_SERIAL
-  Serial.print(", ");   
-  Serial.print(supplyvoltage);
-#endif // ECHO_TO_SERIAL
+  #if ECHO_TO_SERIAL
+    Serial.print(", ");   
+    Serial.print(supplyvoltage);
+  #endif // ECHO_TO_SERIAL
 
   logfile.println();
-#if ECHO_TO_SERIAL
-  Serial.println();
-#endif // ECHO_TO_SERIAL
+  #if ECHO_TO_SERIAL
+    Serial.println();
+  #endif // ECHO_TO_SERIAL
 
   digitalWrite(greenLEDpin, LOW);
 
