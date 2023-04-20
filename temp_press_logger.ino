920f961ff2a3719f9f28f9a237e3ea70f238a92d
Ralph Stevenson-Jones <ralph-sj@users.noreply.github.com>
2023-04-20 21:33:15 +0000

    Added itehr code back in and it works...

--- temp_press_logger.ino
+++ temp_press_logger.ino
@@ -11,9 +11,9 @@
 uint32_t syncTime = 0; // time of last sync(
 uint32_t m;
 #define ECHO_TO_SERIAL      1 
-#define WAIT_TO_START       0 
-#define PRINT_TO_FILE       0 
-#define SET_TIME            0 
+#define WAIT_TO_START       1 
+#define PRINT_TO_FILE       1 
+#define SET_TIME            1 
 #define READ_TEMPERATURE    1 
 #define READ_PRESSURE       1 
 #define TC_ADDRESS_1        (0x60)
@@ -66,27 +66,329 @@ void setup(void)
   Serial.begin(9600);
   Serial.println();
 
+  if (! RTC.begin()) 
+  {
+    error("RTC failed");
+  }
   
+  #if SET_TIME
+    RTC.adjust(DateTime(2023, 4, 20, 21, 40, 0)); // takes about 8 seconds to save the time
+  //  RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  #endif //SET_TIME
+  RTC.start();
+  
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+  
+  #if WAIT_TO_START
+    Serial.println("Type any character to start");
+    while (!Serial.available());
+  #endif //WAIT_TO_START
+
+  starttime  = RTC.now();
+  Serial.print("Logger started at ");
+  Serial.print(starttime.year(), DEC);
+  if (starttime.month() < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(starttime.month(), DEC);
+  if (starttime.day() < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(starttime.day(), DEC);
+  Serial.print("_");
+  if (starttime.hour() < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(starttime.hour(), DEC);
+  if (starttime.minute() < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(starttime.minute(), DEC);
+  if (starttime.second() < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(starttime.second(), DEC);
+  Serial.println();
+
+#if PRINT_TO_FILE
+    // initialize the SD card
+    Serial.print("Initializing SD card...");
+    // make sure that the default chip select pin is set to
+    // output, even if you don't use it:
+    pinMode(10, OUTPUT);
+  
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+      error("Card failed, or not present");
+    }
+    Serial.println("card initialized.");
+  
+    // create a new file
+    char filename[] = "LOGGER00.CSV";
+    for (uint8_t i = 0; i < 100; i++) {
+      filename[6] = i/10 + '0';
+      filename[7] = i%10 + '0';
+      if (! SD.exists(filename)) {
+        // only open a new file if it doesn't exist
+        logfile = SD.open(filename, FILE_WRITE); 
+        break;  // leave the loop!
+      }
+    }
+  
+    if (! logfile) {
+      error("couldnt create file");
+    }
+
+    Serial.print("Logging to: ");
+    Serial.println(filename);
+
+    logfile.print(starttime.year(), DEC);
+    if (starttime.month() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(starttime.month(), DEC);
+    if (starttime.day() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(starttime.day(), DEC);
+    logfile.print("_");
+    if (starttime.hour() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(starttime.hour(), DEC);
+    if (starttime.minute() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(starttime.minute(), DEC);
+    if (starttime.second() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(starttime.second(), DEC);
+    logfile.println();
+    logfile.println("datetime,temp_cold, temp_hot, temp_cold2, temp_hot2, pressure1, pressure2, millis");
+  #else //PRINT_TO_FILE
+    Serial.println("WARNING: DATA NOT LOGGED TO FILE");
+    Serial.println();
+    delay(1000);
+  #endif
+  
+
+  #if ECHO_TO_SERIAL
+    Serial.println("datetime,temp_cold, temp_hot, temp_cold2, temp_hot2, pressure1, pressure2, millis");
+  #endif //ECHO_TO_SERIAL
+ 
+  // Thermocouple
+  #if READ_TEMPERATURE
+    Serial.println("MCP9600 HW test");
+      
+    // TC1
+    /* Initialise the driver with I2C_ADDRESS and the default I2C bus. */
+    if (! mcp.begin(TC_ADDRESS_1)) {
+        Serial.println("Sensor 1 not found. Check wiring!");
+        while (1);
+    }
+    mcp.setADCresolution(MCP9600_ADCRESOLUTION_18);
+    Serial.print("ADC resolution set to ");
+    switch (mcp.getADCresolution()) {
+      case MCP9600_ADCRESOLUTION_18:   Serial.print("18"); break;
+      case MCP9600_ADCRESOLUTION_16:   Serial.print("16"); break;
+      case MCP9600_ADCRESOLUTION_14:   Serial.print("14"); break;
+      case MCP9600_ADCRESOLUTION_12:   Serial.print("12"); break;
+    }
+    Serial.println(" bits");
+
+    mcp.setThermocoupleType(MCP9600_TYPE_K);
+    Serial.print("Thermocouple type set to ");
+    switch (mcp.getThermocoupleType()) {
+      case MCP9600_TYPE_K:  Serial.print("K"); break;
+      case MCP9600_TYPE_J:  Serial.print("J"); break;
+      case MCP9600_TYPE_T:  Serial.print("T"); break;
+      case MCP9600_TYPE_N:  Serial.print("N"); break;
+      case MCP9600_TYPE_S:  Serial.print("S"); break;
+      case MCP9600_TYPE_E:  Serial.print("E"); break;
+      case MCP9600_TYPE_B:  Serial.print("B"); break;
+      case MCP9600_TYPE_R:  Serial.print("R"); break;
+    }
+    Serial.println(" type");
+
+    mcp.setFilterCoefficient(3);
+    Serial.print("Filter coefficient value set to: ");
+    Serial.println(mcp.getFilterCoefficient());
+
+    mcp.enable(true);
+
+    // TC2
+    /* Initialise the driver with I2C_ADDRESS and the default I2C bus. */
+    if (! mcp2.begin(TC_ADDRESS_2)) {
+        Serial.println("Sensor 1 not found. Check wiring!");
+        while (1);
+    }
+    mcp2.setADCresolution(MCP9600_ADCRESOLUTION_18);
+    Serial.print("ADC resolution set to ");
+    switch (mcp2.getADCresolution()) {
+      case MCP9600_ADCRESOLUTION_18:   Serial.print("18"); break;
+      case MCP9600_ADCRESOLUTION_16:   Serial.print("16"); break;
+      case MCP9600_ADCRESOLUTION_14:   Serial.print("14"); break;
+      case MCP9600_ADCRESOLUTION_12:   Serial.print("12"); break;
+    }
+    Serial.println(" bits");
+
+    mcp2.setThermocoupleType(MCP9600_TYPE_K);
+    Serial.print("Thermocouple type set to ");
+    switch (mcp.getThermocoupleType()) {
+      case MCP9600_TYPE_K:  Serial.print("K"); break;
+      case MCP9600_TYPE_J:  Serial.print("J"); break;
+      case MCP9600_TYPE_T:  Serial.print("T"); break;
+      case MCP9600_TYPE_N:  Serial.print("N"); break;
+      case MCP9600_TYPE_S:  Serial.print("S"); break;
+      case MCP9600_TYPE_E:  Serial.print("E"); break;
+      case MCP9600_TYPE_B:  Serial.print("B"); break;
+      case MCP9600_TYPE_R:  Serial.print("R"); break;
+    }
+    Serial.println(" type");
+
+    mcp2.setFilterCoefficient(3);
+    Serial.print("Filter coefficient value set to: ");
+    Serial.println(mcp2.getFilterCoefficient());
+
+    mcp2.enable(true);
+
+    Serial.println("Found MCP9600!");
+  #endif //READ_TEMPERATURE
 }
 
 void loop(void){
-  
+  DateTime now;
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+  digitalWrite(greenLEDpin, HIGH);
+
+  #if READ_TEMPERATURE
+    temp_cold = mcp.readAmbient();
+    temp_hot = mcp.readThermocouple();
+    temp_cold2 = mcp2.readAmbient();
+    temp_hot2 = mcp2.readThermocouple();
+  #endif //READ_TEMPERATURE 
   #if READ_PRESSURE
     PressureVolt1 = analogRead(PressurePin1);  
     PressureVolt2 = analogRead(PressurePin2);  
     Pressure1 = (PressureVolt1 * PressureScale)/AdcRange;
     Pressure2 = (PressureVolt2 * PressureScale)/AdcRange;
   #endif //READ_PRESSURE
-
+  
+  #if PRINT_TO_FILE    
+    logfile.print(now.year(), DEC);
+    if (now.month() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(now.month(), DEC);
+    if (now.day() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(now.day(), DEC);
+    logfile.print("_");
+    if (now.hour() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(now.hour(), DEC);
+    if (now.minute() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(now.minute(), DEC);
+    if (now.second() < 10)
+    {
+      Serial.print("0");
+    }
+    logfile.print(now.second(), DEC);
+    logfile.print(",");
+    logfile.print(temp_cold);
+    logfile.print(",");
+    logfile.print(temp_hot);
+    logfile.print(",");
+    logfile.print(temp_cold2);
+    logfile.print(",");
+    logfile.print(temp_hot2);
+    logfile.print(",");    
+    logfile.print(Pressure1);
+    logfile.print(",");    
+    logfile.print(Pressure2);
+    logfile.print(",");    
+    m = millis();
+    logfile.print(m);           // milliseconds since start
+    logfile.println();
+  #endif //PRINT_TO_FILE
  
   #if ECHO_TO_SERIAL
-   
+    Serial.print(now.year(), DEC);
+    if (now.month() < 10)
+    {
+      Serial.print("0");
+    }
+    Serial.print(now.month(), DEC);
+    if (now.day() < 10)
+    {
+      Serial.print("0");
+    }
+    Serial.print(now.day(), DEC);
+    Serial.print("_");
+    if (now.hour() < 10)
+    {
+      Serial.print("0");
+    }
+    Serial.print(now.hour(), DEC);
+    if (now.minute() < 10)
+    {
+      Serial.print("0");
+    }
+    Serial.print(now.minute(), DEC);
+    if (now.second() < 10)
+    {
+      Serial.print("0");
+    }
+    Serial.print(now.second(), DEC);
+    Serial.print(",");
+    Serial.print(temp_cold);
+    Serial.print(",");
+    Serial.print(temp_hot);
+    Serial.print(",");
+    Serial.print(temp_cold2);
+    Serial.print(",");
+    Serial.print(temp_hot2);
+    Serial.print(",");     
     Serial.print(PressureVolt1);
     Serial.print(",");    
     Serial.print(PressureVolt2);
+    Serial.print(",");    
+    m = millis();
+    Serial.print(m);           // milliseconds since start
     Serial.println();
   #endif //ECHO_TO_SERIAL
 
 
-  delay(1000);
+  digitalWrite(greenLEDpin, LOW);
+
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+  
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  logfile.flush();
+  digitalWrite(redLEDpin, LOW);
 }
